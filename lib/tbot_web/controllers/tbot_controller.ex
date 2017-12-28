7919ccee7095ff317995d2eb3b336df84b44c17f
Daniel Bastos <daniel.bastos1710@gmail.com>
2017-12-28 11:52:28 -0200

    Add post endpoint and POC messenger return value

--- lib/tbot_web/controllers/tbot_controller.ex
+++ lib/tbot_web/controllers/tbot_controller.ex
@@ -1,5 +1,6 @@
 defmodule TbotWeb.TbotController do
   use TbotWeb, :controller
+  import Tbot.MessengerHelper
 
   def challenge(conn,
     %{"hub.mode" => "subscribe", "hub.verify_token" => token, "hub.challenge" => challenge}) do
@@ -9,7 +10,13 @@ defmodule TbotWeb.TbotController do
       conn |> put_status(500)
     end
   end
-
   def challenge(conn, _), do: conn |> put_status(500)
+
+  def webhook(conn, %{"entry" => entry, "object" => "page"}) do
+    parsed_entry = parse_messenger_entry(entry)
+    conn |> send_resp(200, Poison.encode!(parsed_entry))
+  end
+  def webhook(conn, _),  do: conn |> put_status(500)
+
   defp verify_token, do: Application.get_env(:tbot, :messenger_verify_token)
 end
