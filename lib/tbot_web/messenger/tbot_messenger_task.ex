659ee11785b0f84706f3d8c5c022864c9c5bd04d
Daniel Bastos <daniel.bastos1710@gmail.com>
2018-01-02 21:56:09 -0200

    Remove comment in Tbot.MessengerOutputTask

--- lib/tbot_web/messenger/tbot_messenger_task.ex
+++ lib/tbot_web/messenger/tbot_messenger_task.ex
@@ -7,7 +7,6 @@ defmodule Tbot.MessengerOutputTask do
   alias Tbot.MessengerOutput, as: MessengerOutput
 
   def respond_messenger(parsed_entry) do
-    # NOTE: Not sure if "async_nolink" is the best supervisor option
     Task.Supervisor.async_nolink Tbot.TaskSupervisor, fn ->
       body = MessengerOutput.build_request_body(parsed_entry)
       MessengerOutput.send(body)
