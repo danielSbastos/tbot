56d1d641d803a990aa74aadeb2b3db3f228c6a4a
Daniel Bastos <daniel.bastos1710@gmail.com>
2018-01-28 19:50:23 -0200

    Add alternative to get the chosen word coming from a txt file

--- lib/tbot_web/tbot_sync_user_chosen_word.ex
+++ lib/tbot_web/tbot_sync_user_chosen_word.ex
@@ -10,11 +10,31 @@ defmodule Tbot.SyncUserChosenWord do
 
   def sync(magic_map) do
     conn = Redis.start_link
-    word = HangmanWord.fetch_random_english_word |> HangmanWord.translate_to_portuguese
+
+    # You can alter the function below and use word_from_api() instead.
+    word = word_from_file
 
     Agent.start_link
     Agent.update(magic_map.sender_id, :first_interaction)
 
     Redis.set(conn, magic_map.sender_id, :chosen_word, word)
+  end
+
+  defp word_from_file() do
+    word_list = word_file_content |> String.split |> Enum.drop(-1)
+    random_list_index = Enum.random(0..(length(word_list) -1))
+    Enum.at(word_list, random_list_index)
+  end
+
+  defp word_from_api() do
+    HangmanWord.fetch_random_english_word |> HangmanWord.translate_to_portuguese
+  end
+
+  defp word_file_content() do
+    file = Path.join(System.cwd, "/lib/tbot_web/data/word_list.txt")
+    {:ok, pid} = File.open(file)
+    {:ok, content} = File.read(file)
+    File.close(pid)
+    content
   end
 end
